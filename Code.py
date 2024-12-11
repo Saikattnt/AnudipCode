4f6b7dfd225d90420643d80050338082c5cd154b
Saikat <139097941+Saikattnt@users.noreply.github.com>
2024-12-11 11:57:28 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,7 +3,9 @@ def sum_of_digits(number):
     number = abs(number)
     # Calculate the sum of digits
     return sum(int(digit) for digit in str(number))
-
+    #str(number) converts the number into a string
+    #for digit in str(number) iterates over each digit in the string representation of the number.
+    #int(digit) converts the character back into an integer
 
 num = int(input("Enter a number: "))
 print(f"The sum of the digits is: {sum_of_digits(num)}")
